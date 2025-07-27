f4c9e6297721b642e5a3ab02b7b10f70b1da13fe
woramet0 <93065399+woramet0@users.noreply.github.com>
2025-07-27 20:05:25 +0700

    Add files via upload

---
+++ animal.java
@@ -0,0 +1,13 @@
+public abstract class Animal {
+    private final String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract String makeSound();
+}
